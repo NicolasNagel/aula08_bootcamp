1cdbf9b21a8f6802562116476a20bc2a08c555b3
Nicolas Nagel <nicolascnagel@gmail.com>
2025-06-18 11:54:56 -0300

    second commit

---
+++ etl.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+import glob
+
+# Uma função de Extract que lê e consolida os JSON
+
+pasta = 'data'
+arquivos_json = glob.glob(os.path.json(pasta, '*.json'))
+print(arquivos_json)
+
+# Uma função que transforma
+
+# Uma função que da load em csv
